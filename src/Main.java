688271ce1da8b9980b3c471c6d0f581beb4e6fc8
Christian Lisangola <jochri3@gmail.com>
2023-10-12 11:52:10 +0300

    Point 1 : Type number

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Entiers
+        byte age=12;
+        short ageDeMonPere=30000;
+        int monAge=12;
+
+        // Double
+        float prix=25.6f;
+        double prixUht=45.3;
+
+    }
+}
